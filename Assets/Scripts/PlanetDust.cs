c519e294971e414ea26e617cc14234245a92d7e3
pannic <pannic1984@outlook.com>
2023-03-01 22:00:37 +0000

    1

--- Assets/Scripts/PlanetDust.cs
+++ Assets/Scripts/PlanetDust.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using DefaultNamespace;
 using UnityEngine;
 
 public class PlanetDust : MonoBehaviour
 {
+    public List<PlanetDustProperties> gravityPoints;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +14,11 @@ public class PlanetDust : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    void UpdateParticle()
     {
         
     }
---
+++ Assets/Scripts/PlanetDustProperties.cs
@@ -0,0 +1,15 @@
+﻿namespace DefaultNamespace
+{
+    public class PlanetDustProperties
+    {
+        public float AttractionPoint;
+        public float LochLimitPoint;
+        public float Attractiveness;
+        public float AdjustRatio;
+
+        public PlanetDustProperties()
+        {
+            
+        }
+    }
+}
